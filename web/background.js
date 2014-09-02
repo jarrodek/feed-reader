a961c1038a1e216bbf162060120114f8981028ed
jarrodek <jarrodek@gmail.com>
2014-09-02 22:21:53 +0200

    The app is fixed and working! :)

--- web/background.js
+++ web/background.js
@@ -292,7 +292,7 @@ rss.app.showNotifications = function(){
             };
         }
         options = {
-            type: "basic",
+            type: "list",
             title: "You have "+itemsCount+" new articles to read!",
             message: feedsCount + ' has been updated.',
             iconUrl: icon
@@ -343,18 +343,21 @@ rss.app.createAppWindow = function(initialFeed) {
 };
 
 rss.app.init = function() {
+    console.log('Initializing the background app.');
+    rss.app._setupOpenHandlers();
+    rss.app._setupCloseHandlers();
     rss.config.restore(function(){
         rss.app._setupAlarm();
         rss.app._setupNotifications();
-        rss.app._setupCloseHandlers();
-        rss.app._setupOpenHandlers();
         rss.app.update();
     });
 };
 
 rss.app._setupAlarm = function() {
-    chrome.alarms.onAlarm.addListener(rss.app.onAlarm);
-    chrome.alarms.create('feedownload', {'periodInMinutes': rss.config.refreshRate.current});
+    chrome.alarms.clear('feedownload', function(){
+        chrome.alarms.onAlarm.addListener(rss.app.onAlarm);
+        chrome.alarms.create('feedownload', {'periodInMinutes': rss.config.refreshRate.current});
+    });
 };
 
 rss.app._setupNotifications = function() {
@@ -384,8 +387,6 @@ rss.app._setupOpenHandlers = function() {
 rss.app.onAlarm = function(alarm){
     if (alarm && alarm.name === 'feedownload') {
         rss.app.update();
-    } else {
-        console.log('no alarm');
     }
 };
 
